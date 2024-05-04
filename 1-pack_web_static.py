229271a20bccc8a0c2f27d96424c9e2799977b5c
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-05-04 05:26:36 +0100

    second commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Fab file for generating arhived file of webstatic directory"""
+
+
+from datetime import datetime
+from fabric.api import local, env
+from os import makedirs
+
+
+def generate_archive_name():
+    """
+    Generates a unique name for the archive based on the current date and time.
+
+    Returns:
+        str: The name of the archive.
+    """
+    now = datetime.now()
+    return f"web_static_{now.strftime('%Y%m%d%H%M%S')}.tgz"
+
+
+def do_park():
+    """
+    Creates a .tgz archive from the contents of the web_static directory.
+
+    Returns:
+        str: The name of the archive on success, None if failed.
+    """
+    file_name = generate_archive_name()
+    makedirs("versions", exist_ok=True)
+    status = local(f"tar -cvzf versions/{file_name} web_static")
+
+    if status.failed:
+        return None
+    return file_name
